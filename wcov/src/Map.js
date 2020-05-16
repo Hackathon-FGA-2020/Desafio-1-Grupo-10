38c8e31982c16c24120974079d9bfcd0fef6e68c
Caio <caioaa@droidunb.com>
2020-05-16 16:11:17 -0300

    Testes e testes

---
+++ wcov/src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Map extends React.Component {
+	render() {
+		return (
+			<h1> Hello, earth.</h1>
+		);
+	}
+}
+
+export default Map;
--- wcov/src/index.js
+++ wcov/src/index.js
@@ -4,10 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+var newElem = React.createElement('h1',{className: 'greeting'}, 'Hello, earth.');
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
 
