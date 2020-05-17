7eb124b1fd867baa6792878dd25808d9f16e3c5d
Caio <caioaa@droidunb.com>
2020-05-17 20:42:29 -0300

    Inicio dos marcadores

--- wcov/src/components/Map/index.js
+++ wcov/src/components/Map/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Map, TileLayer, Marker, Popup } from 'react-leaflet'
+import { Map, TileLayer, Marker, Popup, ZoomControl, ScaleControl } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 
 import './style.css'
@@ -9,7 +9,7 @@ import { iconFarmacia } from '../../Icon'
 const position = [-15.8276, -47.9218]
 
 
-//VV Tegitntando fazer funcinoar : calc(~'100vh - 100px') 
+//VV Tentando fazer funcinoar : calc(~'100vh - 100px') 
 const mapSize = {
   height: "95vh",
   zIndex: 1
@@ -17,25 +17,38 @@ const mapSize = {
 
 function MapContainer(){
 
+	const [markers, setMarkers] = React.useState([[-15.8276, -47.9218]]);
+
+
+	const addmarker = (e) => {
+		const umarkers = markers
+		const{lat, lng} = e.latlng
+		const newPos = [[lat, lng]]
+		umarkers.push(newPos)
+		setMarkers(umarkers)
+	}
 
   function handleCardInfo(){}
 
   return (
       <div>
-      <Map center={position} zoom={11} style={mapSize} >
+	  <Map 
+	    center={position} zoom={11} 
+		style={mapSize}
+		onClick={addmarker} 
+      >
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution="&copy; <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
           />
+		{markers.map((position) =>
+		  <Marker key={'marker-${idx}'} position={position}>
+			<Popup>
+            	<span>A pretty CSS3 popup. <br/> Easily customizable.</span>
+          	</Popup>
+		  </Marker>
+		)}
 
-          <Marker position={position} 
-          icon={iconFarmacia}
-          
-          draggable
-          onclick={handleCardInfo}
-          >
-          <Popup>Farmacia</Popup>
-          </Marker>
 
       </Map>
       </div>
