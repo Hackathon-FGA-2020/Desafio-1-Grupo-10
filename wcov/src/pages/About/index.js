a3cd6fe4ea9f5a571ddc311b39c3f5aaad63c9b2
Gabriel <gabrielbaliza1@gmail.com>
2020-05-21 02:16:45 -0300

    Ajustes na aparencia da página Sobre (precisa melhorar)

--- wcov/src/pages/About/index.js
+++ wcov/src/pages/About/index.js
@@ -9,8 +9,11 @@ function About (){
   return (
   <>
     <Header/>
-      <h1 className="title"> Sobre </h1>
-      <p className="text"> Aplicação desenvolvida por alunos de graduação da Universidade de Brasília para o Hackathon UnB-FGA 2020, com o obetivo de auxíliar no combate ao COVID-19 trazendo informação para a população.</p>
+      <div class ="container">
+        <h1 className="title"> Sobre </h1>
+        <p className="text"> Aplicação desenvolvida por alunos de graduação da Universidade de Brasília para o Hackathon UnB-FGA 2020, com o obetivo de auxíliar no combate ao COVID-19 trazendo informação para a população.</p>
+      </div>
+     
   </>
   )
 }
